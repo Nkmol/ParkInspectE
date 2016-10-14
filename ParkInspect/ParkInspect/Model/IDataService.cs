9d4345e1328fd7d7c8784fcc3e4ce2e08c1920e3
Nkmol <sandermol95@hotmail.com>
2016-10-14 17:52:57 +0200

    Init MVVM Light project

---
+++ ParkInspect/ParkInspect/Model/IDataService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ParkInspect.Model
+{
+    public interface IDataService
+    {
+        void GetData(Action<DataItem, Exception> callback);
+    }
+}
