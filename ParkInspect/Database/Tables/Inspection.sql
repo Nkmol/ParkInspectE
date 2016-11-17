783552d04ccf19fe5d0b2656262fcf288ca91286
Jasper <jaspe47@hotmail.com>
2016-11-17 19:52:21 +0100

    Database publish script (#15)
    * Database publish script
    
    + edmx
    + publish script
    + datacontext models
    + database tabels
    
    = missing post-deployment-scripts
    = missing database data
    
    - removed herobrine
    
    * Database added
    
    + cpied database in root
    + added database in solution
    
    * fix on sanders comment
    
    * edited sln
    
    + ParkInspect.sln changed
    
    * Update .gitignore
    
    Add ignorance of `.jfm` database file.
    
    * English version
    
    + renamed all .sql files to english verions
    + renamed all collomns
    + renamed all foreign keys
    
    * Fix table and column naming/translation
    
    * Add password attribute
    
    * Addition fixes
    
    * Add new EDMX
    
    * Build edmx

---
+++ ParkInspect/Database/Tables/Inspection.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[Inspection]
+(
+	[id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [parking_id] INT NOT NULL, 
+    [form_id] INT NULL, 
+    [follow_up_id] INT NULL, 
+    [state] VARCHAR(45) NOT NULL, 
+    [deadline] DATETIME NULL, 
+    [date] DATETIME NULL, 
+    [clarification] TEXT NULL, 
+    [assigment_client_id] INT NOT NULL, 
+    [assignment_id] INT NOT NULL, 
+    CONSTRAINT [FK_Inspectie_Parkeerplaats] FOREIGN KEY ([parking_id]) REFERENCES [Parkinglot]([id]), 
+    CONSTRAINT [FK_Inspectie_Vragenlijst] FOREIGN KEY ([form_id]) REFERENCES [Form]([id]), 
+    CONSTRAINT [FK_Inspectie_Inspectie] FOREIGN KEY ([follow_up_id]) REFERENCES [Inspection]([id]), 
+    CONSTRAINT [FK_Inspectie_Status] FOREIGN KEY ([state]) REFERENCES [State]([state]), 
+    CONSTRAINT [FK_Inspectie_Opdracht] FOREIGN KEY ([assigment_client_id], [assignment_id]) REFERENCES [Asignment]([client_id], [id]),
+)
