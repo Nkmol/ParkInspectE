4ee229ae9a500ac1107bb4d9b9a1fdcf7a8e6892
Nkmol <sandermol95@hotmail.com>
2016-11-18 16:16:10 +0100

    Fix naming + build of POCO

--- ParkInspect/Database/Tables/Field.sql
+++ ParkInspect/Database/Tables/Field.sql
@@ -3,9 +3,9 @@
 	[title] VARCHAR(50) NOT NULL, 
     [template_id] INT NOT NULL, 
     [datatype] VARCHAR(50) NOT NULL, 
-    [repportFieldType_title] VARCHAR(50) NULL, 
+    [reportFieldType_title] VARCHAR(50) NULL, 
 	PRIMARY KEY ([title],[template_id]),
     CONSTRAINT [FK_Field_Template] FOREIGN KEY ([template_id]) REFERENCES Template([id]), 
     CONSTRAINT [FK_Field_Datatype] FOREIGN KEY ([datatype]) REFERENCES Datatype([datatype]), 
-    CONSTRAINT [FK_Field_RapportFieldType] FOREIGN KEY ([repportFieldType_title]) REFERENCES ReportFieldType([title])
+    CONSTRAINT [FK_Field_RapportFieldType] FOREIGN KEY ([reportFieldType_title]) REFERENCES ReportFieldType([title])
 )
