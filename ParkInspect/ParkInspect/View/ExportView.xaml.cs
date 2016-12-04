7876419d29bb733953ae2c4893ce9be974cbb00a
bramvdzee <bramvdzee1@gmail.com>
2016-12-04 20:17:53 +0100

    Start

---
+++ ParkInspect/ParkInspect/View/ExportView.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+using ParkInspect.ViewModel;
+
+namespace ParkInspect
+{
+    /// <summary>
+    /// Interaction logic for ExportView.xaml
+    /// </summary>
+    public partial class ExportView : MetroWindow
+    {
+        /// <summary>
+        /// Initializes a new instance of the MainWindow class.
+        /// </summary>
+        public ExportView()
+        {
+        }
+
+
+
+    }
+}
