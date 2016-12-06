456871f54460f51e9cbf8c3813dd2eb3985a20e6
joybr <joybr@LAPTOP-LJB7EVG3>
2016-12-06 12:40:15 +0100

    Develop db

--- ParkInspect/Database/Tables/Contactman.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Contactperson]
-(
-	[id] INT NOT NULL IDENTITY , 
-    [client_id] INT NOT NULL, 
-    [firstname] VARCHAR(50) NOT NULL, 
-    [lastname] VARCHAR(50) NOT NULL, 
-    CONSTRAINT [FK_Client_Contactperson] FOREIGN KEY ([client_id]) REFERENCES Client([id]), 
-    PRIMARY KEY ([client_id], [id])
-)
