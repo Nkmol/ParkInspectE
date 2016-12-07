0e2f61ecf9d8da0f7e309219d032d858b226676c
jamjam <j.witwit@student.avans.nl>
2016-12-07 18:09:57 +0100

    Added zip, phone, email validation

---
+++ ParkInspect/ParkInspect/Model/ValidationRules/IsValidEmailValidationRule.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ParkInspect.Model.ValidationRules
+{
+    public class IsValidEmailValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            bool isEmail = 
+                Regex.IsMatch(Convert.ToString(value), @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z", RegexOptions.IgnoreCase);
+
+            return isEmail ? new ValidationResult(true, null) : new ValidationResult(false, "Geen geldige email!");
+
+        }
+    }
+}
