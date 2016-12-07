2096c78e9823076f179b770edfe37940d82eb609
jordibenck <jordi.b-98@hotmail.com>
2016-12-07 17:36:22 +0100

    Added filters

--- ParkInspect/ParkInspect/ViewModel/ClientViewModel.cs
+++ ParkInspect/ParkInspect/ViewModel/ClientViewModel.cs
@@ -1,29 +1,40 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Windows;
 using System.Windows.Input;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
+using ParkInspect.Model.Factory.Builder;
 using ParkInspect.Repository;
 using ParkInspect.Services;
+using ParkInspect.Model.Factory;
 
 namespace ParkInspect.ViewModel
 {
     public class ClientViewModel : ViewModelBase
     {
+        private string _emailFilter;
+
+        private string _nameFilter;
+        private string _phoneFilter;
         private Client _selectedClient;
+
+        private readonly IEnumerable<Client> Data;
 
         protected ClientService Service;
 
         public ClientViewModel(IRepository context)
         {
             Service = new ClientService(context);
-            Clients = new ObservableCollection<Client>(Service.GetAllClients());
             CompleteClientCommand = new RelayCommand(CompleteClient, CanCreate);
             ResetButtonCommand = new RelayCommand(Reset);
             UpdateButtonCommand = new RelayCommand(UpdateClient, CanUpdate);
             SelectedClient = new Client();
             Assignments = new ObservableCollection<Asignment>(SelectedClient.Asignments);
             Contactpersons = new ObservableCollection<Contactperson>(SelectedClient.Contactpersons);
+            Data = Service.GetAllClients();
+            UpdateClients();
         }
 
         public ObservableCollection<Client> Clients { get; set; }
@@ -31,6 +42,37 @@ namespace ParkInspect.ViewModel
         public ObservableCollection<Asignment> Assignments { get; set; }
 
         public ObservableCollection<Contactperson> Contactpersons { get; set; }
+
+        public string NameFilter
+        {
+            get { return _nameFilter; }
+            set
+            {
+                _nameFilter = value;
+                UpdateClients();
+            }
+        }
+
+        public string PhoneFilter
+        {
+            get { return _phoneFilter; }
+            set
+            {
+                _phoneFilter = value;
+                UpdateClients();
+            }
+        }
+
+        public string EmailFilter
+        {
+            get { return _emailFilter; }
+            set
+            {
+                _emailFilter = value;
+                UpdateClients();
+            }
+        }
+
 
         public Client SelectedClient
         {
@@ -64,6 +106,7 @@ namespace ParkInspect.ViewModel
             Clients.Add(SelectedClient);
             CompleteClientCommand.RaiseCanExecuteChanged();
             UpdateButtonCommand.RaiseCanExecuteChanged();
+            UpdateClients();
 
             MessageBox.Show("Klant toegevoegd");
         }
@@ -78,8 +121,22 @@ namespace ParkInspect.ViewModel
             Service.UpdateClient(SelectedClient);
             CompleteClientCommand.RaiseCanExecuteChanged();
             UpdateButtonCommand.RaiseCanExecuteChanged();
+            UpdateClients();
 
             MessageBox.Show("Klant geupdate");
+        }
+
+        private void UpdateClients()
+        {
+            var builder = new FilterBuilder();
+            builder.Add("name", NameFilter);
+            builder.Add("phonenumber", PhoneFilter);
+            builder.Add("email", EmailFilter);
+
+            var result = Data.Where(x => x.Like(builder.Get()));
+
+            Clients = new ObservableCollection<Client>(result);
+            RaisePropertyChanged("Clients");
         }
     }
 }
