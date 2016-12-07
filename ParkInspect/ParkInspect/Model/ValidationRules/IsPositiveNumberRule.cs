7cb0bc64aa6e965c5d4324399e85322707249cda
jamjam <j.witwit@student.avans.nl>
2016-12-07 19:29:40 +0100

    Validation rule that checks if number is not negative

---
+++ ParkInspect/ParkInspect/Model/ValidationRules/IsPositiveNumberRule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ParkInspect.Model.ValidationRules
+{
+    public class IsPositiveNumberRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (value != null && value is int)
+            {
+                if((int) value > 0)
+                    return new ValidationResult(true, null);
+            }
+
+            return new ValidationResult(false, "Het moet een getal hoger dan 0 zijn!");
+        }
+    }
+}
