02533b5b2b894aa34cf064b8e1b3cc8baff43e69
jamjam <j.witwit@student.avans.nl>
2016-12-07 19:44:59 +0100

    Added DateTime validation

---
+++ ParkInspect/ParkInspect/Model/ValidationRules/IsValidDateTimeRule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ParkInspect.Model.ValidationRules
+{
+    public class IsValidDateTimeRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            DateTime temp;
+            if (DateTime.TryParse(Convert.ToString(value), out temp))
+                return new ValidationResult(true, null);
+
+            return new ValidationResult(false, "Voer een geldige datum in!");
+        }
+    }
+}
