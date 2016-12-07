3a795eb8e31c6e864b3752a4f7cb297375f8b418
Nkmol <sandermol95@hotmail.com>
2016-12-07 14:30:18 +0100

    Removed and improved redundant code

--- ParkInspect/ParkInspect/ViewModel/DashboardViewModel.cs
+++ ParkInspect/ParkInspect/ViewModel/DashboardViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Security.RightsManagement;
 using System.Windows.Documents;
@@ -17,140 +19,16 @@ namespace ParkInspect.ViewModel
     /// </summary>
     public class DashboardViewModel : ViewModelBase
     {
+        private const int AmountTabs = 8;
+
         private readonly IDataService _dataService;
         private MainWindow dashboardWindow;
         private EmployeeService _employeeService;
-
-
-
         private List<Role> PossibleRoles { get; set; }
-
-
 
         public int Height = 100;
         public int Width = 100;
-
-
-
-
-       // list of tabs is a lits of booleans used to show or hide tabs
-
-        #region List of tabs
-        private bool _showAbsence;
-
-        public bool ShowAbsence
-        {
-            get
-            {
-                return _showAbsence;
-            }
-            set
-            {
-                _showAbsence = value;
-                base.RaisePropertyChanged();
-            }
-        }
-
-        private bool _showRaport;
-        public bool ShowRaport
-        {
-            get
-            {
-                return _showRaport;
-            }
-            set
-            {
-                _showRaport = value;
-                base.RaisePropertyChanged();
-            }
-        }
-
-        private bool _showEmployees;
-        public bool ShowEmployee
-        {
-            get
-            {
-                return _showEmployees;
-            }
-            set
-            {
-                _showEmployees = value;
-                base.RaisePropertyChanged();
-            }
-        }
-
-        private bool _showForms;
-        public bool ShowForms
-        {
-            get
-            {
-                return _showForms;
-            }
-            set
-            {
-                _showForms = value;
-                base.RaisePropertyChanged();
-            }
-        }
-
-        private bool _showInspections;
-        public bool ShowInspections
-        {
-            get
-            {
-                return _showInspections;
-            }
-            set
-            {
-                _showInspections = value;
-                base.RaisePropertyChanged();
-            }
-        }
-
-        private bool _showAssignments;
-        public bool ShowAssignments
-        {
-            get
-            {
-                return _showAssignments;
-            }
-            set
-            {
-                _showAssignments = value;
-                base.RaisePropertyChanged();
-            }
-        }
-
-        private bool _showParkinglots;
-        public bool ShowParkinglots
-        {
-            get
-            {
-                return _showParkinglots;
-            }
-            set
-            {
-                _showParkinglots = value;
-                base.RaisePropertyChanged();
-            }
-        }
-
-        private bool _showClients;
-        public bool ShowClients
-        {
-            get
-            {
-                return _showClients;
-            }
-            set
-            {
-                _showClients = value;
-                base.RaisePropertyChanged();
-            }
-        }
-        #endregion
-
-
+        public ObservableCollection<bool> TabsStatus { get; set; }
 
         /// <summary>
         /// Initializes a new instance of the MainViewModel class.
@@ -158,12 +36,17 @@ namespace ParkInspect.ViewModel
         /// 
         public DashboardViewModel(IRepository repository)
         {
+            TabsStatus = new ObservableCollection<bool>(Enumerable.Repeat(true, AmountTabs)); // Init with a Capacity and Default value
 
-
-            ShowDefaultTabs();
             _employeeService = new EmployeeService(repository);
             PossibleRoles = _employeeService.GetAllRoles().ToList();
 
+        }
+
+        private void ResetTabsState()
+        {
+            for (var i = 0; i < TabsStatus.Count; i++)
+                TabsStatus[i] = true;
         }
 
 
@@ -171,107 +54,35 @@ namespace ParkInspect.ViewModel
         {
             PossibleRoles = _employeeService.GetAllRoles().ToList();
 
-            if (PossibleRoles.Contains(role))
+            switch (role?.role1)
             {
-                switch (role.role1)
-                {
-                    case "Employee":
-                        ChangeAuthorizationToEmployee();
-                        break;
-                    case "Inspector":
-                        ChangeAuthorizationToInspector();
-                        break;
-                    case "Manager":
-                        ChangeAuthorizationToManager();
-                        break;
-                    default:
-                        ShowAllTabs();
-                        break;
-                }
+                case "Employee":
+                    ChangeAuthorizationToEmployee();
+                    break;
+                case "Inspector":
+                    ChangeAuthorizationToInspector();
+                    break;
+                default:
+                    ResetTabsState();
+                    break;
             }
-            else
-            {
-                ShowDefaultTabs();
-            }
-
-
         }
 
         private void ChangeAuthorizationToEmployee()
         {
-            ShowAssignments = true;
-            ShowForms = true;
-            ShowClients = true;
-            ShowInspections = true;
-            ShowParkinglots = true;
-            ShowRaport = false;
-            ShowEmployee = false;
-            ShowAbsence = true;
-        }
-
-        private void ChangeAuthorizationToManager()
-        {
-            ShowAssignments = true;
-            ShowForms = true;
-            ShowClients = true;
-            ShowInspections = true;
-            ShowParkinglots = true;
-            ShowRaport = true;
-            ShowEmployee = true;
-            ShowAbsence = true;
+            ResetTabsState();
+            TabsStatus[5] = false;
+            TabsStatus[6] = false;
         }
 
         private void ChangeAuthorizationToInspector()
         {
-            ShowAssignments = false;
-            ShowForms = true;
-            ShowClients = false;
-            ShowInspections = true;
-            ShowParkinglots = true;
-            ShowRaport = false;
-            ShowEmployee = false;
-            ShowAbsence = true;
+            ResetTabsState();
+            TabsStatus[0] = false;
+            TabsStatus[2] = false;
+            TabsStatus[5] = false;
+            TabsStatus[6] = false;
         }
-
-
-        // Waring: security flaw, will be triggered with a known role object with unknown role status.
-        private void ShowAllTabs()
-        {
-            ShowAssignments = true;
-            ShowForms = true;
-            ShowClients = true;
-            ShowInspections = true;
-            ShowParkinglots = true;
-            ShowRaport = true;
-            ShowEmployee = true;
-            ShowAbsence = true;
-        }
-
-        private void ShowDefaultTabs()
-        {
-            ShowAssignments = false;
-            ShowForms = false;
-            ShowClients = false;
-            ShowInspections = true;
-            ShowParkinglots = true;
-            ShowRaport = false;
-            ShowEmployee = false;
-            ShowAbsence = false;
-        }
-        // security lockdown. hides all functionality exept login
-        private void HideAllTabs()
-        {
-            ShowAssignments = false;
-            ShowForms = false;
-            ShowClients = false;
-            ShowInspections = false;
-            ShowParkinglots = false;
-            ShowRaport = false;
-            ShowEmployee = false;
-            ShowAbsence = false;
-        }
-
-
 
     }
 }
