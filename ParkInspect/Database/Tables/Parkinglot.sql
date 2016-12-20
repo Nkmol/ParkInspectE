e17d20887a22156d177d21681539aef3e1c3af77
NanoBob <bhooff@avans.nl>
2016-12-20 21:55:18 +0100

    Database changes
    https://github.com/Nkmol/ParkInspectE/issues/80
    https://github.com/Nkmol/ParkInspectE/issues/72

--- ParkInspect/Database/Tables/Parkinglot.sql
+++ ParkInspect/Database/Tables/Parkinglot.sql
@@ -3,8 +3,9 @@
 	[id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [region_name] VARCHAR(50) NULL, 
     [zipcode] VARCHAR(10) NULL, 
-    [number] INT NULL, 
+    [number] VARCHAR(50) NULL, 
     [name] VARCHAR(50) NULL, 
+    [streetnaame] VARCHAR(50) NULL, 
     [clarification] TEXT NOT NULL, 
     CONSTRAINT [FK_Parkinglot_Region] FOREIGN KEY ([region_name]) REFERENCES [Region]([name]),
 	UNIQUE([name])
