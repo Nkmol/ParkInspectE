77beacc961fae20c68995a1ad7d2ce05bf3dab38
J W <J W>
2016-12-22 16:08:18 +0100

    Fixes Customer
    - Removed the disabled tabs Overview & Management
    - Added validation rule to the customer name field
    
    Closes #119, #116

--- ParkInspect/ParkInspect/ViewModel/DashboardViewModel.cs
+++ ParkInspect/ParkInspect/ViewModel/DashboardViewModel.cs
@@ -315,9 +315,6 @@ namespace ParkInspect.ViewModel
             TabStatus[5] = false;
             TabStatus[6] = false;
             TabStatus[7] = false;
-            TabStatus[15] = false;
-            TabStatus[16] = false;
-
         }
 
 
