580438502de2cc1de529dc5e0c7edb22e22b7cea
bpzee <bpzee@student.avans.nl>
2017-01-29 17:51:54 +0100

    Fix/syncfusion (#203)
    * Report
    
    * Margin fixes. Top row was below header
    
    * Inspectie rapport gemaakt. Link gelegd met Opdrachten tab
    
    * Reports inclusion

--- ParkInspect/ParkInspect/View/ReportDesignView.xaml.cs
+++ ParkInspect/ParkInspect/View/ReportDesignView.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -11,6 +13,10 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using Syncfusion.RDL.DOM;
 using Syncfusion.Windows.Reports.Designer;
 
 namespace ParkInspect
@@ -23,6 +29,38 @@ namespace ParkInspect
         public ReportDesignView()
         {
             InitializeComponent();
+
+            Designer.ReportLoaded += (sender, args) =>
+            {
+
+                var stream = Designer.GetReportStream();
+                
+
+                //Serialize the contents from the file stream to ReportDefinition to edit the ReportDefinition
+                XElement rdl = XElement.Load(XmlReader.Create(stream));
+                string Namespace = (from attribute in rdl.Attributes() where attribute.Name.LocalName == "xmlns" select attribute.Value).FirstOrDefault();
+                XmlSerializer xs = new XmlSerializer(typeof(Syncfusion.RDL.DOM.ReportDefinition), Namespace);
+                Syncfusion.RDL.DOM.ReportDefinition report;
+                using (StringReader reader = new StringReader(rdl.ToString()))
+                {
+                    report = (Syncfusion.RDL.DOM.ReportDefinition)xs.Deserialize(reader);
+                }
+
+                DataSource datasrc = new DataSource();
+                datasrc.Name = "ParkInspect";
+                datasrc.ConnectionProperties = new ConnectionProperties();
+                datasrc.ConnectionProperties.DataProvider = "SQL";
+                datasrc.ConnectionProperties.ConnectString = "Data Source=bhooff.database.windows.net;Initial Catalog=ParkInspect;user id=bhooff;password=Wachtwoord123";
+                datasrc.ConnectionProperties.IntegratedSecurity = true;//for windows authentication.
+
+                if (report.DataSources == null)
+                {
+                    report.DataSources = new DataSources();
+                }
+
+                report.DataSources.Add(datasrc);
+
+            };
         }
     }
 }
