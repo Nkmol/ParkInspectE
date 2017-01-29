d0433c6a053431b85f57eba71e19e73b8b36843f
bramvdzee <bramvdzee1@gmail.com>
2017-01-29 18:50:40 +0100

    Filterfactory is correct met datetime in datagrids, Ontbrekende filters bij Employee toegevoegd, want de layout zag er niet uit zonder.

--- ParkInspect/ParkInspect/Model/Factory/FilterFactory.cs
+++ ParkInspect/ParkInspect/Model/Factory/FilterFactory.cs
@@ -49,7 +49,7 @@ namespace ParkInspect.Model.Factory
         {
 
             if (obj is DateTime)
-                return ((DateTime)obj).ToString("dd/MM/yyyy H:mm:ss tt").Replace('-', '/');
+                return ((DateTime)obj).ToString("MM/dd/yyyy H:mm:ss tt").Replace('-', '/');
 
             return Convert.ToString(obj);
 
