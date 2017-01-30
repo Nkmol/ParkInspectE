09d5f04bc582019a7a01f0089001f2eacf8d038c
jordibenck <jordi.b-98@hotmail.com>
2017-01-30 20:45:52 +0100

    Revert "Broke them into 2 vm's"
    This reverts commit d8b601d7f0690313ff25bf019ab8efe65101d56a.

---
+++ ParkInspect/ParkInspect/ViewModel/EmployeeViewModel.cs
@@ -0,0 +1,357 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Input;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using ParkInspect.Model.Factory;
+using ParkInspect.Model.Factory.Builder;
+using ParkInspect.Repository;
+using ParkInspect.Services;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using ParkInspect;
+
+namespace ParkInspect.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that a View can data bind to.
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class EmployeeViewModel : ViewModelBase
+    {
+        //Service
+        protected EmployeeService Service { get; set; }
+        private string oldPass;
+
+        public ObservableCollection<Absence> Absences { get; set; }
+
+        //Fields and Properties
+        private string _notification;
+
+        public string Notification
+        {
+            get { return _notification; }
+            set
+            {
+                _notification = value;
+                base.RaisePropertyChanged();
+            }
+        }
+
+        #region Filters
+        private string _firstNameFilter;
+
+        public string FirstNameFilter
+        {
+            get
+            {
+                return _firstNameFilter;
+            }
+            set
+            {
+                _firstNameFilter = value;
+                Filter();
+            }
+        }
+
+        private string _lastNameFilter;
+
+        public string LastNameFilter
+        {
+            get
+            {
+                return _lastNameFilter;
+            }
+            set
+            {
+                _lastNameFilter = value;
+                Filter();
+            }
+        }
+
+        private string _phoneFilter;
+
+        public string PhoneFilter
+        {
+            get
+            {
+                return _phoneFilter;
+            }
+            set
+            {
+                _phoneFilter = value;
+                Filter();
+            }
+        }
+
+        private string _emailFilter;
+
+        public string EmailFilter
+        {
+            get
+            {
+                return _emailFilter;
+            }
+            set
+            {
+                _emailFilter = value;
+                Filter();
+            }
+        }
+
+        private string _statusFilter;
+
+        public string StatusFilter
+        {
+            get
+            {
+                return _statusFilter;
+            }
+            set
+            {
+                _statusFilter = value;
+                Filter();
+            }
+        }
+
+        private string _roleFilter;
+
+        public string RoleFilter
+        {
+            get
+            {
+                return _roleFilter;
+            }
+            set
+            {
+                _roleFilter = value;
+                Filter();
+            }
+        }
+
+        private string _inServiceFilter;
+
+        public string InServiceFilter
+        {
+            get
+            {
+                return _inServiceFilter;
+            }
+            set
+            {
+                _inServiceFilter = value;
+                Filter();
+            }
+        }
+
+        private string _outServiceFilter;
+
+        public string OutServiceFilter
+        {
+            get
+            {
+                return _outServiceFilter;
+            }
+            set
+            {
+                _outServiceFilter = value;
+                Filter();
+            }
+        }
+
+        private string _activeFilter;
+
+        public string ActiveFilter
+        {
+            get
+            {
+                return _activeFilter;
+            }
+            set
+            {
+                _activeFilter = value;
+                Filter();
+            }
+        }
+
+        #endregion
+
+
+        private DialogManager _dialog;
+
+        //Data Employees
+        private ObservableCollection<Employee> _employees;
+        public ObservableCollection<Employee> Employees
+        {
+            get { return _employees; }
+            set
+            {
+                _employees = value;
+                base.RaisePropertyChanged();
+            }
+        }
+
+        private IEnumerable<Employee> Data { get; set; }
+
+        private Employee _selectedEmployee;
+        public Employee SelectedEmployee
+        {
+            get { return _selectedEmployee; }
+            set
+            {
+                Set(ref _selectedEmployee, value);
+                base.RaisePropertyChanged();
+                if (_selectedEmployee != null)
+                {
+                    oldPass = _selectedEmployee.password;
+            }
+
+                if(value != null)
+                    Absences = new ObservableCollection<Absence>(SelectedEmployee.Absences.ToList());
+        }
+        }
+
+        //Commands
+        public ICommand SaveCommand { get; set; }
+        public ICommand DeselectEmployeeCommand { get; set; }
+
+        public EmployeeViewModel(IRepository context, DialogManager dialog)
+        {
+            _dialog = dialog;
+            //Service and employees
+            Service = new EmployeeService(context);
+            Employees = new ObservableCollection<Employee>(Service.GetAllEmployees());
+            Data = Service.GetAllEmployees();
+
+            //Initialize startup Employee
+            SelectedEmployee = new Employee();
+            SelectedEmployee.in_service_date = DateTime.Today;
+            SelectedEmployee.out_service_date = DateTime.Today;
+
+            //Initialize commands
+            SaveCommand = new RelayCommand(SaveEmployee);
+            DeselectEmployeeCommand = new RelayCommand(SetNewEmployee);
+        }
+
+
+        //CRU METHODS
+        private void SaveEmployee()
+        {
+            bool error = false;
+
+            //Boolean: employee activity
+            //List: statusses
+            Dictionary<bool, List<string>> statusses = new Dictionary<bool, List<string>>()
+            {
+                {
+                    true, new List<string>()
+                    {
+                        "Retired",
+                        "Terminated"
+                    }
+                },
+                {
+                    false, new List<string>()
+                    {
+                        "Available",
+                        "On Non-Pay leave",
+                        "Suspended",
+                        null
+                    }
+                }
+            };
+
+            Dictionary<bool, List<string>> notifications = new Dictionary<bool, List<string>>()
+            {
+                {
+                    true, new List<string>()
+                    {
+                        "Een medewerker kan niet 'actief' zijn als hij/zij met pensioen is.",
+                        "Een medewerker kan niet 'actief' zijn als hij/zij ontslagen is."
+                    }
+                },
+                {
+                    false, new List<string>()
+                    {
+                        "Een medewerker kan niet 'beschikbaar' zijn als hij/zij geen lopend contract heeft.",
+                        "Een medewerker kan niet 'Op betaald verlof' zijn als hij/zij geen lopend contract heeft.",
+                        "Een medewerker kan niet 'Geschorst' zijn als hij/zij geen lopend contract heeft.",
+                        "Er moet een datum uit dienst ingevoerd worden"
+                    }
+                }
+            };
+
+            if (statusses[SelectedEmployee.active].Contains(SelectedEmployee.employee_status))
+            {
+                var id = statusses[SelectedEmployee.active].IndexOf(SelectedEmployee.employee_status);
+                Notification = notifications[SelectedEmployee.active][id];
+                error = true;
+            }
+
+            if (!error)
+            {
+
+                if (SelectedEmployee.id == 0)
+                {
+                    Service.Add(SelectedEmployee);
+                }
+                else
+                {
+                    Service.Update(SelectedEmployee);
+                    Notification = "De medewerker is aangepast";
+                }
+
+                _dialog.ShowMessage("Gelukt!", Notification);
+                UpdateDataGrid();
+                return;
+            }
+
+            _dialog.ShowMessage("Fout opgetreden", Notification);
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the PersoneelViewModel class.
+        /// </summary>
+
+        private void UpdateDataGrid()
+        {
+            _employees = new ObservableCollection<Employee>(Service.GetAllEmployees());
+            var temp = Employees;
+            Employees = null;
+            Employees = temp;
+            SetNewEmployee();
+        }
+
+        private void Filter()
+        {
+            var builder = new FilterBuilder();
+            builder.Add("firstname", FirstNameFilter);
+            builder.Add("lastname", LastNameFilter);
+            builder.Add("phonenumber", PhoneFilter);
+            builder.Add("in_service_date", InServiceFilter);
+            builder.Add("out_service_date", OutServiceFilter);
+            builder.Add("email", EmailFilter);
+            builder.Add("active", ActiveFilter);
+            builder.Add("role", RoleFilter);
+            builder.Add("employee_status", StatusFilter);
+
+            var result = Data.Where(x => x.Like(builder.Get()));
+
+            Employees = new ObservableCollection<Employee>(result);
+            RaisePropertyChanged("Employees");
+        }
+
+        private void SetNewEmployee()
+        {
+            Employee e = new Employee();
+            e.in_service_date = DateTime.Today;
+            e.out_service_date = DateTime.Today;
+
+            SelectedEmployee = e;
+        }
+    }
+}
