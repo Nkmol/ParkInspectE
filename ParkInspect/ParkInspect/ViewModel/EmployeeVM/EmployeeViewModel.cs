d2cb7f780fc6e05e924fbe0fbc55c0eaa14bdba5
jordibenck <jordi.b-98@hotmail.com>
2017-02-05 19:42:31 +0100

    Fixed the opslaan button

--- ParkInspect/ParkInspect/ViewModel/EmployeeVM/EmployeeViewModel.cs
+++ ParkInspect/ParkInspect/ViewModel/EmployeeVM/EmployeeViewModel.cs
@@ -8,28 +8,16 @@ using ParkInspect.Services;
 namespace ParkInspect.ViewModel.EmployeeVM
 {
     /// <summary>
-    /// This class contains properties that a View can data bind to.
-    /// <para>
-    /// See http://www.galasoft.ch/mvvm
-    /// </para>
+    ///     This class contains properties that a View can data bind to.
+    ///     <para>
+    ///         See http://www.galasoft.ch/mvvm
+    ///     </para>
     /// </summary>
     public class EmployeeViewModel : ViewModelBase
     {
-        //Service
-        protected EmployeeService Service { get; set; }
-
-        private readonly Employee Data;
-
-        public bool InspectorSelected => FormRole?.role1 == "Inspecteur";
-
-        public bool RegionSelected => FormRole?.role1 == "Inspecteur" && FormRegion != null;
-
         private readonly DialogManager _dialog;
 
-        public string Message { get; set; }
-
-        //Commands
-        public RelayCommand<EmployeeOverviewViewModel> SaveCommand { get; set; }
+        private readonly Employee Data;
 
         public EmployeeViewModel(IRepository context, Employee data, DialogManager dialog)
         {
@@ -40,10 +28,155 @@ namespace ParkInspect.ViewModel.EmployeeVM
             //Initialize startup Employee
             Data.in_service_date = DateTime.Today;
             Data.out_service_date = DateTime.Today;
-            
+
             SaveCommand = new RelayCommand<EmployeeOverviewViewModel>(Save);
             Reset();
+        }
+
+        //Service
+        protected EmployeeService Service { get; set; }
+
+        public bool InspectorSelected => FormRole?.role1 == "Inspecteur";
+
+        public bool RegionSelected => (FormRole?.role1 == "Inspecteur") && (FormRegion != null);
+
+        public bool CanSave
+            =>
+            (FormRole != null) &&
+            (((FormRole.role1 == "Inspecteur") && !string.IsNullOrWhiteSpace(FormRegion?.name)) ||
+             (FormRole.role1 != "Inspecteur"));
+
+        public string Message { get; set; }
+
+        //Commands
+        public RelayCommand<EmployeeOverviewViewModel> SaveCommand { get; set; }
+
+        private void SaveForm()
+        {
+            Firstname = FormFirstname;
+            Lastname = FormLastname;
+            Phonenumber = FormPhonenumber;
+            Email = FormEmail;
+            Password = FormPassword;
+            InServiceDate = FormInServiceDate;
+            OutServiceDate = FormOutServiceDate;
+            Active = FormActive;
+            Absences = FormAbsences;
+            Inspections = FormInspections;
+            EmployeeStatus = FormEmployeeStatus;
+            Region = FormRegion;
+            Role = FormRole;
+        }
+
+        private void FillForm()
+        {
+            FormFirstname = Firstname;
+            FormLastname = Lastname;
+            FormPhonenumber = Phonenumber;
+            FormEmail = Email;
+            FormPassword = Password;
+            FormInServiceDate = InServiceDate;
+            FormOutServiceDate = OutServiceDate;
+            FormActive = Active;
+            FormAbsences = Absences;
+            FormInspections = Inspections;
+            FormEmployeeStatus = EmployeeStatus;
+            FormRegion = Region;
+            FormRole = Role;
+        }
+
+        public void Reset()
+        {
             FillForm();
+        }
+
+        private void Save(EmployeeOverviewViewModel overview)
+        {
+            if (Data.id <= 0)
+                Add(overview);
+            else
+                Edit();
+
+            overview.NewEmployee();
+        }
+
+        private void Add(EmployeeOverviewViewModel overview)
+        {
+            SaveForm();
+
+            var error = false;
+
+            var statusses = new Dictionary<bool, List<string>>
+            {
+                {
+                    true, new List<string>
+                    {
+                        "gepensioneerd",
+                        "beëindigde dienst"
+                    }
+                },
+                {
+                    false, new List<string>
+                    {
+                        "beschikbaar",
+                        "onbetaald verlof",
+                        "geschorst",
+                        null
+                    }
+                }
+            };
+
+            var notifications = new Dictionary<bool, List<string>>
+            {
+                {
+                    true, new List<string>
+                    {
+                        "Een medewerker kan niet 'actief' zijn als hij/zij met pensioen is.",
+                        "Een medewerker kan niet 'actief' zijn als hij/zij ontslagen is."
+                    }
+                },
+                {
+                    false, new List<string>
+                    {
+                        "Een medewerker kan niet 'beschikbaar' zijn als hij/zij geen lopend contract heeft.",
+                        "Een medewerker kan niet 'Op betaald verlof' zijn als hij/zij geen lopend contract heeft.",
+                        "Een medewerker kan niet 'Geschorst' zijn als hij/zij geen lopend contract heeft.",
+                        "Er moet een datum uit dienst ingevoerd worden"
+                    }
+                }
+            };
+
+            var notification = "";
+
+            if (statusses[Data.active].Contains(Data.Employee_Status1.employee_status1))
+            {
+                var id = statusses[Data.active].IndexOf(Data.Employee_Status1.employee_status1);
+                notification = notifications[Data.active][id];
+                error = true;
+            }
+
+            if (error)
+            {
+                _dialog.ShowMessage("Personeel toevoegen", notification);
+                return;
+            }
+
+            Message = Service.Add(Data)
+                ? "Het personeelslid is toegevoegd!"
+                : "Er is iets misgegaan tijdens het toevoegen.";
+            _dialog.ShowMessage("Personeel toevoegen", Message);
+
+            overview.Employees.Add(this);
+        }
+
+        private void Edit()
+        {
+            SaveForm();
+            Message = Service.Update(Data)
+                ? "Het personeelslid is aangepast!"
+                : "Er is iets misgegaan tijdens het aanpassen.";
+
+            _dialog.ShowMessage("Personeel bewerken", Message);
         }
 
         #region Properties
@@ -60,7 +193,7 @@ namespace ParkInspect.ViewModel.EmployeeVM
             set { Data.Role1 = value; }
         }
 
-        public ParkInspect.Region Region
+        public Region Region
         {
             get { return Data.Region; }
             set { Data.Region = value; }
@@ -133,24 +266,30 @@ namespace ParkInspect.ViewModel.EmployeeVM
         public string FormEmail { get; set; }
 
         private Role _formRole;
+
         public Role FormRole
         {
             get { return _formRole; }
             set
             {
                 _formRole = value;
-                RaisePropertyChanged("InspectorSelected");                
+                RaisePropertyChanged("InspectorSelected");
+                RaisePropertyChanged("RegionSelected");
+                RaisePropertyChanged("CanSave");
             }
         }
 
-        private ParkInspect.Region _formRegion;
-        public ParkInspect.Region FormRegion
+        private Region _formRegion;
+
+        public Region FormRegion
         {
             get { return _formRegion; }
             set
             {
                 _formRegion = value;
+                RaisePropertyChanged("InspectorSelected");
                 RaisePropertyChanged("RegionSelected");
+                RaisePropertyChanged("CanSave");
             }
         }
 
@@ -175,131 +314,5 @@ namespace ParkInspect.ViewModel.EmployeeVM
         public ICollection<Inspection> FormInspections { get; set; }
 
         #endregion
-
-        private void SaveForm()
-        {
-            Firstname = FormFirstname;
-            Lastname = FormLastname;
-            Phonenumber = FormPhonenumber;
-            Email = FormEmail;
-            Password = FormPassword;
-            InServiceDate = FormInServiceDate;
-            OutServiceDate = FormOutServiceDate;
-            Active = FormActive;
-            Absences = FormAbsences;
-            Inspections = FormInspections;
-            EmployeeStatus = FormEmployeeStatus;
-            Region = FormRegion;
-            Role = FormRole;
-        }
-
-        private void FillForm()
-        {
-            FormFirstname = Firstname;
-            FormLastname = Lastname;
-            FormPhonenumber = Phonenumber;
-            FormEmail = Email;
-            FormPassword = Password;
-            FormInServiceDate = InServiceDate;
-            FormOutServiceDate = OutServiceDate;
-            FormActive = Active;
-            FormAbsences = Absences;
-            FormInspections = Inspections;
-            FormEmployeeStatus = EmployeeStatus;
-            FormRegion = Region;
-            FormRole = Role;
-        }
-
-        public void Reset()
-        {
-            FillForm();
-        }
-
-        private void Save(EmployeeOverviewViewModel overview)
-        {
-            if (Data.id <= 0)
-                Add(overview);
-            else
-                Edit();
-
-            overview.NewEmployee();
-        }
-
-        private void Add(EmployeeOverviewViewModel overview)
-        {
-            SaveForm();
-
-            var error = false;
-
-            var statusses = new Dictionary<bool, List<string>>()
-            {
-                {
-                    true, new List<string>()
-                    {
-                        "gepensioneerd",
-                        "beëindigde dienst"
-                    }
-                },
-                {
-                    false, new List<string>()
-                    {
-                        "beschikbaar",
-                        "onbetaald verlof",
-                        "geschorst",
-                        null
-                    }
-                }
-            };
-
-            var notifications = new Dictionary<bool, List<string>>()
-            {
-                {
-                    true, new List<string>()
-                    {
-                        "Een medewerker kan niet 'actief' zijn als hij/zij met pensioen is.",
-                        "Een medewerker kan niet 'actief' zijn als hij/zij ontslagen is."
-                    }
-                },
-                {
-                    false, new List<string>()
-                    {
-                        "Een medewerker kan niet 'beschikbaar' zijn als hij/zij geen lopend contract heeft.",
-                        "Een medewerker kan niet 'Op betaald verlof' zijn als hij/zij geen lopend contract heeft.",
-                        "Een medewerker kan niet 'Geschorst' zijn als hij/zij geen lopend contract heeft.",
-                        "Er moet een datum uit dienst ingevoerd worden"
-                    }
-                }
-            };
-
-            var notification = "";
-
-            if (statusses[Data.active].Contains(Data.Employee_Status1.employee_status1))
-            {
-                var id = statusses[Data.active].IndexOf(Data.Employee_Status1.employee_status1);
-                notification = notifications[Data.active][id];
-                error = true;
-            }
-
-            if (error)
-            {
-                _dialog.ShowMessage("Personeel toevoegen", notification);
-                return;
-            }        
-
-            Message = Service.Add(Data)
-                    ? "Het personeelslid is toegevoegd!"
-                    : "Er is iets misgegaan tijdens het toevoegen.";
-                _dialog.ShowMessage("Personeel toevoegen", Message);
-
-            overview.Employees.Add(this);
-        }
-
-        private void Edit()
-        {
-            SaveForm();
-            Message = Service.Update(Data) ? "Het personeelslid is aangepast!" : "Er is iets misgegaan tijdens het aanpassen.";
-
-            _dialog.ShowMessage("Personeel bewerken", Message);
-        }
     }
 }
